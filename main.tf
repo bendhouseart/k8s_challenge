0916c10fc7987210e08e45ecadfe6310dd0fb324
Khristian Brooks <kh.brooks02@gmail.com>
2020-10-15 17:59:00 -0400

    add aws provider

---
+++ main.tf
@@ -0,0 +1,18 @@
+###################
+# AWS Configuration
+###################
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 2.70"
+    }
+  }
+}
+
+# Define AWS as our provider
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
