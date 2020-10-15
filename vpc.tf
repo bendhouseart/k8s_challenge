01f094323868ff220afad4092e0b93aed4157eff
Khristian Brooks <kh.brooks02@gmail.com>
2020-10-15 14:33:02 -0400

    fix syntax

--- vpc.tf
+++ vpc.tf
@@ -5,24 +5,24 @@ resource "aws_vpc" "kubernetes" {
 }
 
 resource "aws_subnet" "kubernetes" {
-  vpc_id            = "${aws_vpc.kubernetes.id}"
+  vpc_id            = aws_vpc.kubernetes.id
   cidr_block        = "10.43.0.0/16"
   availability_zone = "eu-west-1a"
 }
 
 resource "aws_internet_gateway" "gw" {
-  vpc_id = "${aws_vpc.kubernetes.id}"
+  vpc_id = aws_vpc.kubernetes.id
 }
 
 resource "aws_route_table" "kubernetes" {
-  vpc_id = "${aws_vpc.kubernetes.id}"
+  vpc_id = aws_vpc.kubernetes.id
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = "${aws_internet_gateway.gw.id}"
+    gateway_id = aws_internet_gateway.gw.id
   }
 }
 
 resource "aws_route_table_association" "kubernetes" {
-  subnet_id      = "${aws_subnet.kubernetes.id}"
-  route_table_id = "${aws_route_table.kubernetes.id}"
+  subnet_id      = aws_subnet.kubernetes.id
+  route_table_id = aws_route_table.kubernetes.id
 }
