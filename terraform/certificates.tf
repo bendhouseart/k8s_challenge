51cf14c13709fded75677c258111a2dbf8293810
Anthony Galassi <2317139-anthonygalassi@users.noreply.gitlab.com>
2020-10-19 19:27:53 -0700

    moved certs to ../certs relative to local-exect

--- terraform/certificates.tf
+++ terraform/certificates.tf
@@ -33,9 +33,9 @@ resource "null_resource" "certificates" {
     template_rendered = data.template_file.certificates.rendered
   }
   provisioner "local-exec" {
-    command = "echo '${data.template_file.certificates.rendered}' > cert/kubernetes-csr.json"
+    command = "echo '${data.template_file.certificates.rendered}' > ../cert/kubernetes-csr.json"
   }
   provisioner "local-exec" {
-    command = "cd cert; cfssl gencert -initca ca-csr.json | cfssljson -bare ca; cfssl gencert -ca=ca.pem -ca-key=ca-key.pem -config=ca-config.json -profile=kubernetes kubernetes-csr.json | cfssljson -bare kubernetes"
+    command = "cd ../cert; cfssl gencert -initca ca-csr.json | cfssljson -bare ca; cfssl gencert -ca=ca.pem -ca-key=ca-key.pem -config=ca-config.json -profile=kubernetes kubernetes-csr.json | cfssljson -bare kubernetes"
   }
 }
